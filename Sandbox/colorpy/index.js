1e4c5bb3ac2ac8ffa8d70fe871729965456c2079
tripledrive <56107095+tripledrive@users.noreply.github.com>
2021-08-12 21:47:07 +0900

    Add files via upload

--- Sandbox/colorpy/index.js
+++ Sandbox/colorpy/index.js
@@ -1,19 +1,43 @@
 "use strict";
+var open_flag = false;
+var check_flag = false;
+let answer = [
+    ["オープン","おーぷん","OPEN"],["ごりら"],["らっぱ"]
+];
 
     const Wrapping = Vue.createApp({
         data(){
             return{
                 currentFlag: 0,
+                bugFlag:0,
                 players:[
                     {name:"あああ",img:'img/drsk_icon.png'},
                     {name:"BBB",img:'img/drsk_icon.png'},
-                    {name:"ccc",img:'img/drsk_icon.png'}]
+                    {name:"ccc",img:'img/drsk_icon.png'}],
+                input:""
             }
         },
         computed:{
             currentFlagComp(){
-                console.log('vue:'+this.currentFlag);
+                console.log(this.currentFlag);
+                console.log(this.bugFlag);
                 return 'text_' + String(this.currentFlag);
+            }
+        },
+        methods:{
+            checkanswer(){
+                check_flag = false
+                    for(let i = 0; i < answer[this.currentFlag].length; i++){
+                        if(this.input == answer[this.currentFlag][i]){
+                            open_flag = true
+                            check_flag = true
+                            if(this.currentFlag == 0){
+                                jQuery('.st').css('display','block')
+                            }
+                            break;
+                    }
+                    if(!check_flag){}
+                }
             }
         }
     })    
@@ -91,16 +115,83 @@ Wrapping.component('text_1',{
     </div>
     <hr width="95%" noshade="">`
 })
+Wrapping.component('text_0_x',{
+    data(){
+        return{
+            role:'role_orgn',
+            imgs:'img/drsk_icon.png',
+            subtitle: "ここが、このサーバーの始まりです。",
+            title: "DRSKカラー杯オンラインserverへようこそ"
+        }
+    },
+    template:`
+    <div class="postage">
+    <div class="icon">
+        <img :src="imgs">
+    </div>
+    <div class="text">
+        <span class="name role_orgn">ド███催)</span><br>
+        <span class="text">【大会███要】<br>
+            <br>
+            DRSK██████杯オンラインサーバーに入室頂き誠にありがとうございます。<br>
+            当サーバーは皆様にDRSKカラー杯運███の営むイベントを楽しんで頂くサーバーです。<br>
+            <br>
+            <br>
+            イベントにおき█████████ャーマッチ他、試合風景をビデオ██████を行う場合があります。<br>
+            撮影した映像はYouTubeやTwitterで使用させ███場合がありま███。<br>
+            入賞者のデッ██████はツイッター上で公開させてい███場合がございます。<br>
+            あ███じめご了承ください。</span>
+    </div>
+    </div>
+    <hr width="95%" noshade="">
+    <p id="top_tex">{{subtitle}}</p>
+    <h2 id="top">{{title}}</h2>`
+})
+Wrapping.component('text_1_x',{
+    data(){
+        return{
+            imgs:'img/drsk_icon.png',
+        }
+    },
+    template:`
+    <div class="postage">
+    <div class="icon">
+        <img :src="imgs">
+    </div>
+    <div class="text">
+        <span class="name role_orgn">ドラ██████主催)</span><br>
+        <span class="text">
+        【大会の流れ】<br>
+        <br>
+        ■大会開始前(受付)<br>
+        来場されましたらまず#参加受付 もしくは#参加受付（当日枠用） へ<br>
+        ・Tonamelで入力したエントリー名<br>
+        ・大会詳細███確認の有無(ルール確認し█████████言お願いします)<br>
+        をお送りください。<br>
+        その後運営より「参加者」のロールが付与███れれば██████トリー完了です。<br>
+        <br>
+        <br>
+        ■各試合開始前<br>
+        各予選の対戦組み合わせはTonamelのトーナメント表にて発表しその旨をdiscord内で@参加者宛にアナウンスさせて頂きます。<br>
+        アナウンス█████████れたテーブルにご着席ください。<br>
+        試合開始タイミングは @参加者 で通知いた███。<br>
+        <br>
+        ■各試合終了後<br>
+        勝者の方は#対戦結果受付にてテー███ル番号と結果を書き込んでください。<br>
+        (必ず勝███き込むようお願いします)<br>
+        <br>
+        <br>
+        ■サブイベント<br>
+        謎解きはスイスドロー3回戦終了時より #█████████（step１）にて解禁されます。<br>
+        お手隙の際にぜひお楽しみください</span>
+    </div>
+    </div>
+    <hr width="95%" noshade="">`
+})
 
 Wrapping.mount('#wrap')
 
-
-
-　　// ボタンのDOM要素を取得
 　　var btn = document.getElementsByClassName('btn');
-
-　　// ボタンの個数分ループ
-　　// 変数「i」に現在のループ回数が代入される
 　　for (var i = btn.length - 1; i >= 0; i--) {
 　　　　btnAction(btn[i],i);
 　　}
@@ -121,3 +212,14 @@ Wrapping.mount('#wrap')
 　　　　}
 　　})
 }
+
+jQuery('#testclk').on('click',function(){
+   if(flag){
+      flag = false
+      jQuery('.st').css('display','none')
+    }else{
+        flag = true
+        jQuery('.st').css('display','block')
+    }
+    console.log(flag);
+});
